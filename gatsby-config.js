819354d7e037102f725d77839ba018716e7b6e8b
Peter Koenders <pkoenders@gmail.com>
2021-10-24 02:59:55 +1300

    aa

--- gatsby-config.js
+++ gatsby-config.js
@@ -205,26 +205,37 @@ module.exports = {
     //   },
     // },
 
+    'gatsby-plugin-advanced-sitemap',
+
+    // {
+    //   resolve: 'gatsby-plugin-google-tagmanager',
+    //   options: {
+    //     id: 'GTM-K99LMXQ',
+    //     includeInDevelopment: false,
+    //     enableWebVitalsTracking: true,
+    //   },
+    // },
+
     {
-      resolve: 'gatsby-plugin-google-tagmanager',
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        id: 'GTM-K99LMXQ',
-        includeInDevelopment: false,
-        enableWebVitalsTracking: true,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-XKYVS8LDN0', // Google Analytics / GA
+        ],
+
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+
+          // Avoids sending pageview hits from custom paths
+          exclude: ['/preview/**', '/do-not-track/me/too/'],
+        },
       },
     },
 
     'gatsby-plugin-react-helmet',
-    // {
-    //   resolve: 'gatsby-plugin-robots-txt',
-    //   options: {
-    //     host: 'https://pkoenders.netlify.app',
-    //     sitemap: 'https://pkoenders.netlify.app/sitemap/sitemap-index.xml',
-    //     policy: [{ userAgent: '*', allow: '/' }],
-    //   },
-    // },
-
-    'gatsby-plugin-advanced-sitemap',
 
     {
       resolve: 'gatsby-plugin-robots-txt',
