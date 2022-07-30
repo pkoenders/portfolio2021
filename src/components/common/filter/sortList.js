64dc01d9dbee43ae0e5aebdf620194ef8c485732
Peter Koenders <pkoenders@gmail.com>
2022-07-31 01:23:32 +1200

    sort list update

--- src/components/common/filter/sortList.js
+++ src/components/common/filter/sortList.js
@@ -149,7 +149,8 @@ const Sort = ({
      * @namespace aria
      */
 
-    var aria = aria || {}
+    // var aria = aria || {}
+    var aria = {}
 
     /**
      * @constructor
@@ -888,7 +889,6 @@ const Sort = ({
     function updateSortOrder(focusedItem) {
       sortItemClick(focusedItem)
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
@@ -913,17 +913,18 @@ const Sort = ({
           <IconMaterial icon={'expand_more'} />
         </button>
 
-        <ul
+        <ul // eslint-disable-line jsx-a11y/no-static-element-interactions
           id="list_box"
-          tabIndex="-1"
           role="listbox"
+          tabindex="0"
           aria-labelledby="list_elem"
           className="sortBox hidden"
         >
           {items.map((node, i) => (
-            <li
+            <li // eslint-disable-line jsx-a11y/no-static-element-interactions
               id={'list-item'[i]}
               role="option"
+              tabindex="0"
               aria-selected="false"
               data-nodepath={items[i].nodePath}
               key={'list-item'[i]}
